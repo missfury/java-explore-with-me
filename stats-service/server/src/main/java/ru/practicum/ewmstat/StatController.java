57ff08c1d35c6baaef27023c718bb226943aa2f3
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-09-14 13:42:20 +0300

    fix: some fixes

--- stats-service/server/src/main/java/ru/practicum/ewmstat/StatController.java
+++ stats-service/server/src/main/java/ru/practicum/ewmstat/StatController.java
@@ -30,7 +30,7 @@ public class StatController {
     public Collection<StatsViewDto> getViewStats(
             @RequestParam(value = "start") @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime start,
             @RequestParam(value = "end") @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime end,
-            @RequestParam(value = "uris") List<String> uris,
+            @RequestParam(value = "uris", defaultValue = "") List<String> uris,
             @RequestParam(value = "unique", defaultValue = "false") Boolean unique) {
         log.info("Получена статистика просмотров с временем начала {} временем окончания {}," +
                 " uris {} статусом уникальности ip {}", start, end, uris, unique);
