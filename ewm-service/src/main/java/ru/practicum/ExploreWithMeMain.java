0b4e6f3d406f7b78265dc194c33ca5353aa364c6
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-09-26 13:42:26 +0300

    Fix: some fixes

--- ewm-service/src/main/java/ru/practicum/ExploreWithMeMain.java
+++ ewm-service/src/main/java/ru/practicum/ExploreWithMeMain.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(value= {StatsClient.class})
+@Import(value = {StatsClient.class})
 public class ExploreWithMeMain {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMeMain.class, args);
