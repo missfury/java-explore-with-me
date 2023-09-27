3a383f2d556103b136eebd3d31de978f3a3cd802
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-09-27 12:34:45 +0300

    Fix: Some fixes

--- stats-service/server/src/main/java/ru/practicum/exception/ExceptionsHandler.java
+++ stats-service/server/src/main/java/ru/practicum/exception/ExceptionsHandler.java
@@ -14,13 +14,6 @@ import java.util.Map;
 public class ExceptionsHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleThrowableException(Throwable e) {
-        log.error("Код ошибки: {}, {}", HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        return Map.of("error", "Server Error", "errorMessage", e.getMessage());
-    }
-
-    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationException(ValidationException e) {
         log.error("Error Validation Exception: {}, {}", HttpStatus.BAD_REQUEST, e.getMessage());
